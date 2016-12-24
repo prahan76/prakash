1253019c76fc1516a614b0a80d89b33c474cecf3
prahan76 <prahan76@gmail.com>
2016-12-24 12:30:40 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var httpServer = http.createServer( function(req,res){
-	res.end('this is my first node app');
-});
-
-httpServer.listen(8080, function(){
-	console.log('Server listening on port 8080');
-});
