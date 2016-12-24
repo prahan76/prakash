f4ee9efb64741cfb389d525f37f4930de4eb47c1
prahan76 <prahan76@gmail.com>
2016-12-24 17:12:55 +0000

    Updated port details

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var http = require('http');
-var httpServer = http.createServer( function(req,res){
-	res.end('this is my first node app');
-});
 
-httpServer.listen(8080, function(){
+var http = require('http');
+
+try{
+	var httpServer = http.createServer( function(req,res){
+	res.end('this is my first node app 123');
+});
+} catch(err) {
+	console.log(err);
+}
+
+try
+{
+	httpServer.listen(8081, function(){
 	console.log('Server listening on port 8080');
 });
+} catch(err) {
+	console.log(err);
+}
+
+
